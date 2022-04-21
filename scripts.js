5115841ef6201327071e427f6f2ac26f68cebb2a
madrackwp <gohweipin98@gmail.com>
2022-04-21 22:36:07 +0800

    Done!

--- scripts.js
+++ scripts.js
@@ -1,24 +1,8 @@
-// var check = function() {
-//   if (document.getElementById('password').value ==
-//     document.getElementById('confirm-password').value) {
-//     document.getElementById('message').style.color = 'green';
-//     document.getElementById('message').innerHTML = 'matching';
-//   } else {
-//     document.getElementById('password-message').style.color = 'red';
-//     document.getElementById('password-message').innerHTML = 'not matching';
-//   }
-// }
-
-// $('#password, #confirm-password').on('keyup', function () {
-//   if ($('#password').val() == $('#confirm-password').val()) {
-//     $('#password-message').html('Matching').css('color', 'green');
-//   } else 
-//     $('#password-message').html('Not Matching').css('color', 'red');
-// });
-// console.log("HEllow  owrld")
 const confirmPasswordField = document.querySelector("#confirm-password")
 const passwordField = document.querySelector("input#password")
 const passwordConfirmMessage = document.querySelector("p#password-message")
+const emailField = document.querySelector("input#email")
+
 confirmPasswordField.addEventListener("keyup", ()=>{
   if (passwordField.value != confirmPasswordField.value){
     passwordConfirmMessage.innerHTML = "Passwords do not match!"
@@ -26,3 +10,4 @@ confirmPasswordField.addEventListener("keyup", ()=>{
     passwordConfirmMessage.innerHTML = ""
   }
 })
+
