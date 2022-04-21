03647ee5055a3f605f0dfad5d0086d2a56107441
madrackwp <gohweipin98@gmail.com>
2022-04-21 09:13:06 +0800

    Done with the content and styling, what is left is the verification of fields

--- scripts.js
+++ scripts.js
@@ -9,9 +9,20 @@
 //   }
 // }
 
-$('#password, #confirm-password').on('keyup', function () {
-  if ($('#password').val() == $('#confirm-password').val()) {
-    $('#password-message').html('Matching').css('color', 'green');
-  } else 
-    $('#password-message').html('Not Matching').css('color', 'red');
-});
+// $('#password, #confirm-password').on('keyup', function () {
+//   if ($('#password').val() == $('#confirm-password').val()) {
+//     $('#password-message').html('Matching').css('color', 'green');
+//   } else 
+//     $('#password-message').html('Not Matching').css('color', 'red');
+// });
+// console.log("HEllow  owrld")
+const confirmPasswordField = document.querySelector("#confirm-password")
+const passwordField = document.querySelector("input#password")
+const passwordConfirmMessage = document.querySelector("p#password-message")
+confirmPasswordField.addEventListener("keyup", ()=>{
+  if (passwordField.value != confirmPasswordField.value){
+    passwordConfirmMessage.innerHTML = "Passwords do not match!"
+  } else {
+    passwordConfirmMessage.innerHTML = ""
+  }
+})
